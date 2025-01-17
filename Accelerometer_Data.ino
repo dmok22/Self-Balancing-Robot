754256c23d7a770d6a8cc6dd2df0f45eec327529
hejeffrey2004 <148797770+hejeffrey2004@users.noreply.github.com>
2025-01-16 17:45:18 -0800

    accelerometer_working_code

---
+++ Accelerometer_Data.ino
@@ -0,0 +1,82 @@
+/*
+  Arduino BMI270_BMM150 - Simple Accelerometer
+
+  This example reads the acceleration values from the BMI270_BMM150
+  sensor and continuously prints them to the Serial Monitor
+  or Serial Plotter.
+
+  The circuit:
+  - Arduino Nano 33 BLE Sense Rev2
+
+  created 10 Jul 2019
+  by Riccardo Rizzo
+
+  This example code is in the public domain.
+*/
+
+#include "Arduino_BMI270_BMM150.h"
+#include <stdio.h>
+#include <math.h>
+
+float x, y, z;
+int degreesX = 0;
+int degreesY = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+}
+
+void loop() {
+  char userInput;
+  float x, y, z;
+   userInput = Serial.read();               // read user input
+      
+   if(userInput == 'g'){              //uncomment when testing with python       
+            
+    IMU.readAcceleration(x, y, z);
+
+    //y is ax in the note
+    //z is ay in the note
+
+    if(y >= 0){
+        y = 100*y;
+        //Serial.print("y: ");
+        //Serial.println(y);
+        }
+    if(y < 0){
+        y = 100*y;
+        //Serial.print("y: ");
+        //Serial.println(y);
+        }
+
+    if(z >= 0){
+        z = 100*z;
+       // Serial.print("z: ");
+        //Serial.println(z);
+        }
+    if(z < 0){
+        z = 100*z;
+        //Serial.print("z: ");
+        //Serial.println(z);
+        }
+
+    float thetaRadians = atan2f(y, z);
+    float thetaDegrees = thetaRadians * (180.0f / M_PI); 
+    Serial.println(thetaDegrees);
+   } //uncommnet when using with python
+
+}
+
+  
+
